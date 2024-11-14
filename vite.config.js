3ea2b98e7f42c7eb15448e9dd7ed2548e03c64f3
RomainArcheray <archeray.romain@gmail.com>
2024-11-14 12:35:03 +0100

    Correct error

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite'
 export default defineConfig({
   test: {
     globals: true,
-    environment: 'node', // C'est important d'utiliser 'node' comme environnement de test
+    environment: 'node',
   },
 })
